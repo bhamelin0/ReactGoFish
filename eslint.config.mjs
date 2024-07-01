f7b774bb8d63d6ac01207003eee4507d9df56567
bhamelin0 <bhamelin@uwo.ca>
2024-07-01 15:34:26 -0400

    configure eslint with jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,21 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
+import jest from 'eslint-plugin-jest';
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
+    {files: ["src/**/*.{js,mjs,cjs,jsx}"]},
+    { languageOptions: { 
+        parserOptions: { ecmaFeatures: { jsx: true } } ,
+        globals: {
+        ...globals.browser,
+        ...globals.jest
+    }}},
+    pluginJs.configs.recommended,
+    ...fixupConfigRules(pluginReactConfig),
+    { plugins: { jest }},
+    { rules: {
+      ...jest.configs.recommended.rules,
+    }}
 ];
